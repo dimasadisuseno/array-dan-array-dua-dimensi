71b6d5d89208494d8b40a1f147becfc875fdedc6
dimasadisuseno <57306195+dimasadisuseno@users.noreply.github.com>
2019-11-12 19:11:28 +0700

    Add files via upload

---
+++ Arraydimas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraydimas;
+
+/**
+ *
+ * @author dimaspc
+ */
+public class Arraydimas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     String[] keluarga = {"saman","mustofa","indah","arif","dimas"};
+     System.out.println (keluarga.length);
+    }
+    
+}
