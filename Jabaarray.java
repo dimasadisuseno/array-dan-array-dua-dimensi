525aa23f712858d6cd7f0053cfe2a1af5798b27d
dimasadisuseno <57306195+dimasadisuseno@users.noreply.github.com>
2019-11-12 19:14:58 +0700

    Add files via upload

---
+++ Jabaarray.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jabaarray;
+
+/**
+ *
+ * @author dimaspc
+ */
+public class Jabaarray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String[] karyawan = {"dimas", "eggie","firman","agus","rizky"};
+       System.out.println(karyawan[4]); 
+    }
+}
+
